6373a74de80421b92919ee49275fffa368b30bfa
Kyamu <124717126+Kyamu@users.noreply.github.com>
2023-04-20 22:45:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const removeBtn = document.getElementById("removeBtn");
+removeBtn.addEventListener("click", function() {
+  const select = document.getElementById("colorSelect");
+  const index = select.selectedIndex; // Get the index of the selected option
+  if (index !== -1) { // If an option is selected
+    select.remove(index); // Remove the selected option from the list
+  }
+});
